598f1ed7f13e4e130a665080d464f9c2a3a4beaf
sherwinky <lipeng@feiyuemx.com>
2015-03-20 13:56:04 +0800

    modify sql script

--- src/main/webapp/WEB-INF/sql/0_createdb.sql
+++ src/main/webapp/WEB-INF/sql/0_createdb.sql
@@ -1,4 +1,9 @@
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+	FROM pg_stat_activity
+	WHERE pg_stat_activity.datname = 'address_db'
+		AND pid <> pg_backend_pid();
 DROP DATABASE IF EXISTS address_db;
 DROP USER IF EXISTS addr_user;
 CREATE USER addr_user PASSWORD '123';
 CREATE DATABASE address_db owner addr_user ENCODING = 'UTF-8';
+grant addr_user to postgres;
